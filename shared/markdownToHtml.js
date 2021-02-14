aba946e305a34c170b7c6b3c6c362b5b8b91b900
Thomas Deconinck <thomas.deconinck@colisweb.com>
2021-02-14 23:36:53 +0100

    add remark-external-link plugin

--- shared/markdownToHtml.js
+++ shared/markdownToHtml.js
@@ -3,10 +3,12 @@ import html from "remark-html";
 import prism from "remark-prism";
 import slug from "remark-slug";
 import headings from "remark-autolink-headings";
+import externalLinks from "remark-external-links";
 
 export default async function markdownToHtml(markdown) {
   const result = await remark()
     .use(slug)
+    .use(externalLinks, { target: "_blank", rel: ["noopener", "noreferrer"] })
     .use(headings, {
       behavior: "wrap",
     })
