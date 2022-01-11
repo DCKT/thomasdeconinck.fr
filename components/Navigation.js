7ae202543bdceb3b8eb0d9e966e61b2f36ce1401
Thomas Deconinck <thomas.deconinck@colisweb.com>
2022-01-11 10:45:10 +0100

    feat: Add active style for navigation links & some styles

--- components/Navigation.js
+++ components/Navigation.js
@@ -5,6 +5,7 @@ import { IoMdClose } from "react-icons/io";
 import clsx from "clsx";
 import { useSsr, useLocalStorage, useDarkMode } from "../shared/hooks";
 import { BsSunFill, BsMoonStarsFill } from "react-icons/bs";
+import { useRouter } from "next/router";
 
 const DarkModeToggler = () => {
   const { isServer, isBrowser } = useSsr();
@@ -66,6 +67,7 @@ const DarkModeToggler = () => {
 
 export default function Navigation({ links }) {
   const { isBrower } = useSsr();
+  const router = useRouter();
   let [isMenuVisible, setMenuVisible] = React.useState(false);
 
   React.useEffect(
@@ -82,7 +84,14 @@ export default function Navigation({ links }) {
   return (
     <nav className="relative flex items-center md:items-start justify-between py-8 px-4 lg:px-0 max-w-screen-lg 2xl:max-w-screen-2xl mx-auto">
       <Link href="/" passHref>
-        <a className="font-bold text-2xl menu-link dark:text-white tracking-wider">
+        <a
+          className={clsx(
+            "font-bold text-2xl menu-link dark:text-white tracking-wider",
+            {
+              "menu-link--active": router.asPath === "/",
+            }
+          )}
+        >
           Thomas Deconinck
         </a>
       </Link>
@@ -133,7 +142,14 @@ export default function Navigation({ links }) {
               className="border-t md:border-none w-full md:w-auto dark:border-gray-500"
             >
               <Link href={url} passHref>
-                <a className="tracking-wider text-xl p-4 md:p-0 inline-block h-full md:h-auto menu-link w-full dark:text-gray-300 dark:hover:text-white">
+                <a
+                  className={clsx(
+                    "tracking-wider text-xl p-4 md:p-0 inline-block h-full md:h-auto menu-link w-full dark:text-gray-300 dark:hover:text-white",
+                    {
+                      "menu-link--active": router.asPath === url,
+                    }
+                  )}
+                >
                   {label}
                 </a>
               </Link>
--- pages/index.js
+++ pages/index.js
@@ -95,7 +95,7 @@ export default function Home({ hello, picture, presentation, menu, seo }) {
         </div>
         <Image
           data={picture}
-          className="md:w-[680px] xl:w-[800px] 2xl:w-[850px] mr-0  lg:-mr-40 animate-upScale motion-reduce:transition-none motion-reduce:transform-none"
+          className="md:w-[680px] xl:w-[800px] 2xl:w-[850px] mr-0   animate-upScale motion-reduce:transition-none motion-reduce:transform-none"
         />
       </div>
     </>
