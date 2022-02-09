8f82cc1197e3db36be1ef56a904702aac0942e53
Thomas Deconinck <thomas.deconinck@colisweb.com>
2022-02-09 23:22:33 +0100

    feat: add dynamic SiteMap

---
+++ pages/sitemap.xml.js
@@ -0,0 +1,68 @@
+import { ALL_ARTICLES_QUERY } from "../shared/queries";
+import { request } from "../shared/datocms";
+import { globby } from "globby";
+
+function generateSiteMap({ blogArticlesPaths, labsPaths }) {
+  return `<?xml version="1.0" encoding="UTF-8"?>
+   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+     <!--We manually set the two URLs we know already-->
+     <url>
+       <loc>https://thomasdeconinck.fr</loc>
+     </url>
+     <url>
+       <loc>https://thomasdeconinck.fr/about</loc>
+     </url>
+     ${blogArticlesPaths
+       .map((path) => {
+         return `
+       <url>
+           <loc>${`https://thomasdeconinck.fr/blog${path}`}</loc>
+       </url>
+     `;
+       })
+       .join("")}
+     ${labsPaths
+       .map((path) => {
+         return `
+       <url>
+           <loc>${`https://thomasdeconinck.fr/labs${path}`}</loc>
+       </url>
+     `;
+       })
+       .join("")}
+   </urlset>
+ `;
+}
+
+function Sitemap() {}
+
+export async function getServerSideProps({ res }) {
+  const { articles } = await request({
+    query: ALL_ARTICLES_QUERY,
+  });
+
+  const blogArticlesPaths = articles
+    .map(({ slugs }) => slugs)
+    .flat()
+    .map(
+      ({ locale, slug }) => `${locale === "fr" ? "" : `/${locale}`}/${slug}`
+    );
+
+  const labsPaths = await (
+    await globby(["pages/labs/**/*.js"])
+  ).map((path) => {
+    return path
+      .replace("pages/labs", "")
+      .replace("/index.js", "")
+      .replace(".js", "");
+  });
+
+  res.setHeader("Content-Type", "text/xml");
+
+  res.write(generateSiteMap({ blogArticlesPaths, labsPaths }));
+  res.end();
+
+  return { props: {} };
+}
+
+export default Sitemap;
