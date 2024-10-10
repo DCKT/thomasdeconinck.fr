03dafd92d5cf17ec4d502100519a89be8c7f243f
Thomas Deconinck <thomas.deconinck@colisweb.com>
2024-10-10 22:36:56 +0200

    feat: add aerospace article

--- app/components/posts.tsx
+++ app/components/posts.tsx
@@ -21,11 +21,11 @@ export function BlogPosts() {
             className="flex flex-col space-y-1 mb-4 hover:underline"
             href={`/blog/${post.slug}`}
           >
-            <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2">
-              <p className="text-neutral-600 dark:text-neutral-400 w-[130px] flex-shrink-0 tabular-nums">
+            <div className="w-full flex flex-col md:grid md:grid-cols-3 space-x-0 md:space-x-2">
+              <p className="text-neutral-600 dark:text-neutral-400 tabular-nums">
                 {formatDate(post.metadata.publishedAt, false)}
               </p>
-              <p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
+              <p className="text-neutral-900 dark:text-neutral-100 col-span-2 tracking-tight">
                 {post.metadata.title}
               </p>
             </div>
