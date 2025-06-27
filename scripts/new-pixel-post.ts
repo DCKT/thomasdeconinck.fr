f100509ef61ec1cb205ac20a01030c048ec4ba7c
Thomas Deconinck <thomas.deconinck@colisweb.com>
2025-06-27 09:36:59 +0200

    add new pixel post and fix pixel script

--- scripts/new-pixel-post.ts
+++ scripts/new-pixel-post.ts
@@ -1,28 +1,23 @@
-import fs from "fs";
+import { mkdir, writeFile } from "node:fs/promises";
 import path from "path";
 
 const now = new Date();
-const formattedDate = `${now.getFullYear()}-${now.getMonth() + 1}-${now.getDate() + 1}`;
+const formattedDate = `${now.getFullYear()}-${now.getMonth() + 1 < 10 ? "0" : ""}${now.getMonth() + 1}-${now.getDate() + 1}`;
 const postsDir = path.resolve(process.cwd(), "./app/pixel-art/posts/");
 const publicDir = path.resolve(process.cwd(), "./public/");
+const TEMPLATE = `---
+title: ""
+publishedAt: "${formattedDate}"
+summary: ""
+---
+`;
 
 process.stdout.write("Slug name: ");
+let name = "";
 
 for await (const line of console) {
-  const name = `${formattedDate}-${line}`;
-
-  console.log(path.resolve(publicDir, name));
-  fs.mkdir(path.resolve(publicDir, name), (err) => {
-    if (err) {
-      console.log(`Unable to create folder`);
-      console.log(err);
-    } else {
-      fs.writeFile(path.resolve(postsDir, `${name}.mdx`), "", (err) => {
-        if (err) {
-          console.log(`Unable to create file`);
-        }
-      });
-    }
-  });
-  process.exit(0);
+  name = `${formattedDate}-${line}`;
+  break;
 }
+await mkdir(path.resolve(publicDir, name));
+await writeFile(path.resolve(postsDir, `${name}.mdx`), TEMPLATE);
