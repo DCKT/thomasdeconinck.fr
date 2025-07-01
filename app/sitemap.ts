0e9859f1f7af2defa6cbe6c0f60a9b592ce8e7a9
Thomas Deconinck <thomas.deconinck@colisweb.com>
2025-07-01 09:42:05 +0200

    clean rss/sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,17 +1,21 @@
-import { getBlogPosts } from 'app/blog/utils'
+import { getBlogPosts, getPixelArtPosts } from "app/blog/utils";
 
-export const baseUrl = 'https://portfolio-blog-starter.vercel.app'
+export const baseUrl = "https://www.thomasdeconinck.fr";
 
 export default async function sitemap() {
   let blogs = getBlogPosts().map((post) => ({
     url: `${baseUrl}/blog/${post.slug}`,
     lastModified: post.metadata.publishedAt,
-  }))
+  }));
+  let pixels = getPixelArtPosts().map((post) => ({
+    url: `${baseUrl}/blog/${post.slug}`,
+    lastModified: post.metadata.publishedAt,
+  }));
 
-  let routes = ['', '/blog'].map((route) => ({
+  let routes = ["", "/blog", "/pixel-art"].map((route) => ({
     url: `${baseUrl}${route}`,
-    lastModified: new Date().toISOString().split('T')[0],
-  }))
+    lastModified: new Date().toISOString().split("T")[0],
+  }));
 
-  return [...routes, ...blogs]
+  return [...routes, ...blogs, ...pixels];
 }
