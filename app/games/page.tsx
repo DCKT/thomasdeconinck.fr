69c0dae25b6b5e692b92c57e6a9c3a28aa328eef
Thomas Deconinck <thomas.deconinck@colisweb.com>
2025-07-01 09:26:43 +0200

    update

---
+++ app/games/page.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: "Games",
+  description: "Some of my mini games I developed",
+};
+
+export default function Page() {
+  return (
+    <section>
+      <h1 className="font-semibold text-4xl mb-8 font-title">Games</h1>
+      <p>
+        Sometimes I make some basic video games, I post them here with some
+        thought or feedback about the development.
+      </p>
+    </section>
+  );
+}
