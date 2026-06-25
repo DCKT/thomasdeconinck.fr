7e1f888283ce94a5c614f6cb77b10b4c3872d4a2
Thomas Deconinck <thomas.deconinck@colisweb.com>
2026-06-25 18:51:24 +0200

    fix

---
+++ types/fontsource.d.ts
@@ -0,0 +1,6 @@
+declare module "@fontsource/rubik-doodle-shadow";
+declare module "@fontsource/rubik-doodle-shadow/400.css";
+declare module "@fontsource/rye";
+declare module "@fontsource/rye/400.css";
+declare module "@fontsource/special-elite";
+declare module "@fontsource/special-elite/400.css";
